5452aaa5ac81eb2da19473cdaca6892d5da3464d
Colin Cason <colinjcason@gmail.com>
2020-08-04 17:55:35 -0700

    added cart page and routing

--- cuts/src/components/Header.js
+++ cuts/src/components/Header.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import Box from '@material-ui/core/Box';
 import IconButton from '@material-ui/core/IconButton';
+import Badge from '@material-ui/core/Badge';
+import { Link } from 'react-router-dom';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -25,13 +27,19 @@ export default function Header() {
   const classes = useStyles();
 
   return (
-      <AppBar position="static" className={classes.root}>
+      <AppBar className={classes.root}>
         <Toolbar>
           <Box className={classes.title}>
-            Shop
+              <Link to='/' style={{textDecoration: 'none', color: 'black'}}>
+                Shop
+              </Link>
           </Box>
           <IconButton>
-            <ShoppingCartIcon style={{color: 'black'}} />
+              <Link to='/cart'>
+                  <Badge badgeContent={0} showZero color='primary'>
+                    <ShoppingCartIcon style={{color: 'black'}} />
+                  </Badge>
+              </Link>
           </IconButton>
         </Toolbar>
       </AppBar>
---
+++ cuts/src/components/Home.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import Header from './Header';
+import Grid from '@material-ui/core/Grid';
+import Content from './Content';
+import Box from '@material-ui/core/Box';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  hero: {
+    backgroundImage: 'url(https://images.unsplash.com/photo-1587563871167-1ee9c731aefb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9)',
+    height: '400px',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  header: {
+    fontSize: '4rem',
+  },
+  content: {
+    display: 'flex',
+    justifyContent: 'center',
+    marginTop: '20px',
+  },
+  subHeader: {
+    fontSize: '2rem',
+    fontWeight: 'bold',
+    marginBottom: '15px',
+  },
+  demo: {
+    backgroundImage: 'url(https://images.unsplash.com/photo-1523646745854-e018a9bcc73c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9)',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    height: '350px',
+    margin: '60px'
+  },
+  contact: {
+    height: '500px',
+  },
+});
+
+function Home() {
+  const classes = useStyles();
+  return (
+      <Grid container direction='column'>
+        <Grid item>
+          <Header />
+        </Grid>
+
+        <Box className={classes.hero}>
+          <Box className={classes.header}>
+            Welcome to Kicks
+          </Box>
+        </Box>
+
+        <Grid item container className={classes.content} align='center'>
+        <Grid item xs={4} />
+          <Grid item xs={4} className={classes.subHeader}>
+            Shop
+          </Grid>
+          <Grid xs={4} />
+
+          <Grid item xs={1} sm={0}/>
+          <Grid item xs={10} sm={12} className={classes.products}>
+            <Content />
+          </Grid>
+          <Grid item xs={1} sm={0}/>
+        </Grid>
+
+        {/* <Grid item container justify='center' align='center'>
+          <Grid item xs={4} style={{backgroundColor: 'grey'}}/>
+          <Grid item xs={4} className={classes.subHeader}>
+            Shop Now
+          </Grid>
+          <Grid xs={4} style={{backgroundColor: 'grey'}}/>
+        </Grid> */}
+
+        <Box className={classes.demo}></Box>
+
+        <Box className={classes.contact}></Box>
+
+      </Grid>
+  );
+}
+
+export default Home;
